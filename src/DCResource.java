a7578d23436de59ad0acd280ed1613947c717e21
kshefchek <kshefchek@gmail.com>
2012-02-27 14:22:23 -0500

    Added class DCResource, could not name the class Resource because this conflicts with a Jena class

---
+++ src/DCResource.java
@@ -0,0 +1,33 @@
+import com.hp.hpl.jena.rdf.model.*;
+
+public class DCResource {
+	
+	public String URI;
+	public Model dcModel;
+	
+	
+	public DCResource(String URI){
+		
+		this.URI = URI;
+		this.dcModel = ModelFactory.createDefaultModel();
+		this.dcModel.read(URI);
+		
+	}
+	
+	public Model getRDF(){
+		return dcModel;
+	}
+	
+	public void setURIandRDF(String newURI){
+		URI = newURI;
+		dcModel = ModelFactory.createDefaultModel();
+		dcModel.read(URI);
+	}
+	
+	public String getURI(){
+		return URI;
+	}
+
+}
+
+	
