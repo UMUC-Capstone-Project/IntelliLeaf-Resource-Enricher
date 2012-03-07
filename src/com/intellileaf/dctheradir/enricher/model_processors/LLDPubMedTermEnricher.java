58924f33982956140f3b08eef42ccc64b35c93b4
nlgatewood <nlgatewood@ngatewood>
2012-03-07 18:28:37 -0500

    sparql update

--- src/com/intellileaf/dctheradir/enricher/model_processors/LLDPubMedTermEnricher.java
+++ src/com/intellileaf/dctheradir/enricher/model_processors/LLDPubMedTermEnricher.java
@@ -1,13 +1,21 @@
 package com.intellileaf.dctheradir.enricher.model_processors;
 
-import java.util.List;
+import java.util.ArrayList;
+import com.hp.hpl.jena.rdf.model.*;
+import com.hp.hpl.jena.query.*;
 
-import com.hp.hpl.jena.rdf.model.Model;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class LLDPubMedTermEnricher implements KnowledgeBaseProcessor
 {
-	private Model resultModel;
+	private Model resultModel = ModelFactory.createDefaultModel();
 	private List<Integer> pmids;
+	private List<String> searchTerms;
 
 	public List<Integer> getPMIDs ()
 	{
@@ -30,8 +38,39 @@ public class LLDPubMedTermEnricher implements KnowledgeBaseProcessor
 	@Override
 	public void run ()
 	{
-		// TODO Auto-generated method stub
+
+		// collection of the PubMed IDs retrieved from getPIDs() method
+		//go thru and capture each PubMed ID in the 'for' loop and then incorporate each PubMed ID (one-by-one)...
+		//...into the SPARQL query to retrieve LLD information
+
+		for(int x = 0; x < pmids.size(); x++)
+		{
+			//PID.get(x);
+			//still have to do this part using SPARQL...
+			//...read in the PubMed IDs from the ArrayList one-by-one and queries LLD with SPARQL for each PubMedID*/
+
+			String sparql1=
+					"PREFIX pubmed: <http://linkedlifedata.com/resource/pubmed/> " +
+					"PREFIX lifeskim: <http://linkedlifedata.com/resource/lifeskim/> " +
+					"PREFIX skos: <http://www.w3.org/2004/02/skos/core#> " +
+
+					"SELECT DISTINCT ?concept ?termLabel " +
+						"WHERE {" +
+						"   <https://linkedlifedata.com/resource/pubmed/id/" + pmids.get(x) + "> lifeskim:mentions ?concept." +
+						"   ?concept skos:prefLabel ?termLabel." +
+						" }";
+			
+			Query query = QueryFactory.create(sparql1);
+			QueryExecution qexec = QueryExecutionFactory.sparqlService(
+			"http://linkedlifedata.com/sparql", query);
+			
+			ResultSet results = qexec.execSelect();
+			ResultSetFormatter.out(System.out, results, query);
+			
+			qexec.close() ;
+		}
 
 	}
 
 }
+
