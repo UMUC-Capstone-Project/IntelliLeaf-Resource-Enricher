3fe718b7dddd922fde50f95500c22c21fb5498ad
Marco Brandizi <marco.brandizi@intellileaf.com>
2012-03-08 00:28:50 +0100

    Bugfixing ResourceEnricher.isResourceSupported().

---
+++ src/com/intellileaf/dctheradir/enricher/NS.java
@@ -0,0 +1,15 @@
+package com.intellileaf.dctheradir.enricher;
+
+/**
+ * A facility that contains common namespaces, in the form of string constants. Please note that the string fields
+ * are defined in lower case here, despite the Java convention of using all-upper-case names for constants. That's 
+ * because it is so common to use lower case for RDF/XML namespaces.  
+ *
+ * <dl><dt>date</dt><dd>Mar 8, 2012</dd></dl>
+ * @author brandizi
+ *
+ */
+public class NS
+{
+	public static final String rdf = "http://www.w3.org/1999/02/22-rdf-syntax-ns#";
+}
