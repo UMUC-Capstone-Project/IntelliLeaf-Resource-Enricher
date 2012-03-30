d5548997ff5122face578f223fc125b12ba8bb8a
Marco Brandizi <marco.brandizi@intellileaf.com>
2012-03-30 17:36:55 +0100

    Manual loading of ontology files.

--- src/com/intellileaf/dctheradir/enricher/Resources.java
+++ src/com/intellileaf/dctheradir/enricher/Resources.java
@@ -4,15 +4,18 @@ import com.hp.hpl.jena.ontology.OntModel;
 import com.hp.hpl.jena.ontology.OntModelSpec;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
 
-import java.io.FileInputStream;
+import java.io.File;
+import java.io.FileFilter;
 import java.io.FileNotFoundException;
-import java.io.InputStream;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Resources
 {
 	private static OntModel directoryModel = null;
 	
-	
+	private static Logger log = LoggerFactory.getLogger ( Resources.class );
 	
 	/**
 	 * The DCTHERA Directory model.
@@ -24,11 +27,10 @@ public class Resources
 		if ( directoryModel != null ){
 			return directoryModel;
 		}
-		// Load it from the dump file
 		
+		// Load it from the dump file
 		String URI = "http://artemisia.leafbioscience.com/dcthera_dump.rdf";
 		
-		InputStream input = new FileInputStream("./ontology/dcthera_newontology.owl");
 		
 		/* 
 		 * ResourceEnricher.isResourceSupported() requires a minimum degree of inference, i.e., automatic reasoning. 
@@ -36,14 +38,32 @@ public class Resources
 		 * that's too slow during updates). Likely OWL_MEM is not enough.
 		 * 
 		 */
-		directoryModel =  
-				ModelFactory.createOntologyModel(OntModelSpec.OWL_DL_MEM_TRANS_INF);
-				directoryModel.read(input, "RDF/XML-ABBREV"); 
-				
+		directoryModel = ModelFactory.createOntologyModel(OntModelSpec.OWL_DL_MEM_TRANS_INF);
+		
+		log.info ( "Loading Ontologies" );
+		
+		// Let's go with manual loading, this is less flexible than automatic import-closure, but faster and more reliable. 
+		// Moreover, auto-import would need to remap a few invalid URIs
+		//
+		directoryModel.getDocumentManager ().setProcessImports ( false );
+		File ontoDir = new File ( "./ontology" );
+		File owlFiles[] = ontoDir.listFiles ( new FileFilter() { public boolean accept ( File file ) {
+				return file.getName ().toLowerCase ().endsWith ( ".owl" );
+			}
+		});
+		
+		for ( File owlFile: owlFiles ) {
+			log.info ( "Loading ontology: '" + owlFile.getName () );
+			directoryModel.read ( "file://" + owlFile.getAbsolutePath () );
+		}
+			
+		log.info ( "Ontologies loaded, now the Directory" );
 		directoryModel.read(URI);
 
 		//directoryModel.write(System.out);
 		
+		log.info ( "Knowledge base loaded!" );
+
 		// Then return it
 		return directoryModel;
 	}
